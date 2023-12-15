b18e66eb89f2bc85ff3e023f8217060b21b32878
Ryan <shieldsjryan@gmail.com>
2023-12-14 22:07:38 -0700

    First draft of main code using functions

---
+++ main.r
@@ -0,0 +1,16 @@
+rsf <- read_and_summarize("RSF_Ready_ActiveOnly_Land.csv")
+rsf <- bin_aspect_values(rsf)
+rsf <- convert_to_factors(rsf)
+column_names <- read_column_names("columns_to_scale.txt")
+rsf <- rescale_variables(rsf, column_names)
+
+str(rsf)
+summary(rsf)
+
+# check for multicollinearity
+# For columns 6 to 11
+cor_matrix_1 <- perform_multicollinearity_tests(rsf, 6:11)
+
+# For columns 15 to 19
+cor_matrix_2 <- perform_multicollinearity_tests(rsf, 15:19)
+
