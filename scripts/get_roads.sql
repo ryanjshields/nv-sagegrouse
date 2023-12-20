b717dafca50d0b0964c5f63eec273c5916a80cb8
Ryan <shieldsjryan@gmail.com>
2023-12-20 15:51:49 -0700

    Refactored get_roads.sql to improve performance

--- scripts/get_roads.sql
+++ scripts/get_roads.sql
@@ -1,21 +1,25 @@
-WITH ActiveLeks AS (
-    SELECT ST_Buffer(ST_Transform(geom, 26911), 5000) AS geom
+WITH NVboundary AS (
+    SELECT geom
+    FROM state_boundary
+), FilteredLeks AS (
+    SELECT ST_Buffer(geom, 5000) AS geom
     FROM lek_sites
     WHERE state = 'NV' AND lek_status = 'Active'
+    AND ST_Intersects(geom, (SELECT geom FROM NVboundary))
+), FilteredDistribution AS (
+    SELECT geom
+    FROM distribution
+    WHERE ST_Intersects(geom, (SELECT geom FROM NVboundary))
 ), Combined AS (
     SELECT ST_Union(geom) AS geom FROM (
-        SELECT geom FROM ActiveLeks
+        SELECT geom FROM FilteredLeks
         UNION ALL
-        SELECT ST_Transform(geom, 26911) FROM distribution
+        SELECT geom FROM FilteredDistribution
     ) AS Sub
-), NVboundary AS (
-    SELECT ST_Transform(geom, 26911) AS geom
-    FROM state_boundary
 ), StudyArea AS (
     SELECT ST_Intersection(Combined.geom, NVboundary.geom) AS geom
-    FROM Combined
-    JOIN NVboundary ON ST_Intersects(Combined.geom, NVboundary.geom)
+    FROM Combined, NVboundary
 )
 SELECT roads.*
 FROM roads
-JOIN StudyArea ON ST_Within(ST_Transform(roads.geom, 26911), StudyArea.geom);
+WHERE ST_Within(roads.geom, (SELECT geom FROM StudyArea));
