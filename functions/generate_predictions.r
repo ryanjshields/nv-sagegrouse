f98948dee3f30d7841613e66ed94f9c027c7bceb
Ryan <shieldsjryan@gmail.com>
2023-12-19 22:09:50 -0700

    bug fixes to get the main to execute

--- functions/generate_predictions.r
+++ functions/generate_predictions.r
@@ -1,10 +1,12 @@
 # Function to add predictions and write to CSV
+library(dplyr)
+
 generate_predictions <- function(data, model, filename) {
   # Add predictions column to the data frame
   data$predictions <- predict(model, newdata = data, type = "response")
 
   # Create a new dataframe with just the coordinates and predictions
-  data_predict <- select(data, POINT_X, POINT_Y, predictions)
+  data_predict <- select(data, UTME_NAD83, UTMN_NAD83, predictions)
 
   # Write the new dataframe to a CSV file
   write.csv(data_predict, file = filename)
--- main.r
+++ main.r
@@ -4,38 +4,33 @@ source("functions/bin_aspect.R")
 source("functions/convert_to_factors.R")
 source("functions/read_column_names.R")
 source("functions/rescale_variables.R")
-source("functions/multicollinearity_tests.R")
-source("functions/read_model_formulas.R")
-source("functions/complete_model_analysis.R")
+source("functions/model_selection.R")
+source("functions/collapse_vegetation.R")
+source("functions/generate_predictions.R")
 
-# Read and summarize the data
-rsf <- read_and_summarize("C:\\Users\\spati\\Downloads\\RSF_Ready_ActiveOnly_Land.csv")
+# Read and summarize the datasets
+model_data <- read_and_summarize("D:\\CarverMikiah_rsf_nevada_sagegrouse\\Data\\RSF_Ready_ActiveOnly_Land.csv")
+prediction_surface <- read_and_summarize("D:\\CarverMikiah_rsf_nevada_sagegrouse\\Data\\prediction_surface_sample.csv")
 
 # Bin aspect values
-rsf <- bin_aspect_values(rsf)
+model_data <- bin_aspect_values(model_data)
+prediction_surface <- bin_aspect_values(prediction_surface)
 
-###
 # Convert specified columns to factors
 factor_columns <- read_column_names("function_inputs\\factors_columns.txt")
-rsf <- convert_to_factors(rsf, factor_columns)
+model_data <- convert_to_factors(model_data, factor_columns)
+#prediction_surface <- convert_to_factors(prediction_surface, factor_columns)
 
 # Rescale variables based on column names from a text file
-column_names <- read_column_names("function_inputs\\columns_to_scale.txt")
-rsf <- rescale_variables(rsf, column_names)
-
-# Print the structure and summary of the dataframe
-str(rsf)
-summary(rsf)
-
-# Check for multicollinearity
-cor_matrix_1 <- multicollinearity_tests(rsf, 6:11)
-cor_matrix_2 <- multicollinearity_tests(rsf, 15:19)
+scale_columns <- read_column_names("function_inputs\\columns_to_scale.txt")
+model_data <- rescale_variables(model_data, scale_columns)
+prediction_surface <- rescale_variables(prediction_surface, scale_columns)
 
 # Run model selection and averaging
-formulas <- read_model_formulas("function_inputs\\model_formulas.txt")
-analysis_results <- complete_model_analysis(rsf, formulas, family = "binomial")
+model_avg_results <- model_selection(model_data)
 
-# Check if the model analysis was successful and proceed accordingly
-#if (!is.null(analysis_results)) {
-  # Further analysis or predictions can go here, using `analysis_results$avgmodel`
-#}
+# Collapse the vegetation classes for the sample point grid
+prediction_surface <- collapse_vegetation(prediction_surface, "SAF_SRM")
+
+# Use model avg results to generate prediction values for the sample point grid
+generate_predictions(prediction_surface, model_avg_results, "prediction_surface.csv")
